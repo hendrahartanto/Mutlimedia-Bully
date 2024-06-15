36fe320d01c8825174a27c3d4ec8b6b228e4061b
Zweych <103168091+Zweych@users.noreply.github.com>
2024-06-15 16:21:51 +0700

    Delete Project directory

--- Project/js/homepage.js
+++
@@ -1,6 +0,0 @@
-function toggleVideo(){
-    const trailer = document.querySelector('.trailer');
-    const video = document.querySelector('video');
-    video.pause();
-    trailer.classList.toggle('active');
-}
